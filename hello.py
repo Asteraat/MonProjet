f65180af2b1f4ed7351b5ad92c8248535926b5d2
Asteraat <moa_mehdi_farouche@hotmail.ch>
2026-03-19 20:00:59 +0100

    Mise a jour du fichier avec test ia sur s25

--- hello.py
+++ hello.py
@@ -1,2 +1,14 @@
-print("hello github")
-print("ajouté depuis le pc cloud")
+# Simulation d'une IA Azure Cognitive Services
+confiance = 0.85
+
+print("--- Analyse d'image IA ---")
+if confiance > 0.80:
+    print("Résultat : L'IA est sûre qu'il s'agit d'un chat !")
+elif confiance<81 && confiance>65
+    print("Résultat : L’IA hesite encore")
+else:
+    print("Résultat : Ce n est probablement pas un chat")
+
+# Exercice : Change la valeur de 'confiance' sur ton PC 
+# et fais un 'git pull' sur ton tel pour voir le résultat changer !
+
