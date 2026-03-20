dc065fb4c69104eb1b103890c45ee27881240334
Asteraat <moa_mehdi_farouche@hotmail.ch>
2026-03-20 06:52:30 +0100

    ajout de fonctionalités plus de valeur en dehirs de 0 et 1

--- hello.py
+++ hello.py
@@ -1,7 +1,19 @@
+import time
 # Simulation d'une IA Azure Cognitive Services
-confiance = 0.80
 
+# On boucle "pour toujours" jusqu'à ce qu'on décide de sortir
+while True:
+    confiance = float(input("Valeur de confiance (0 à 1) : "))
+    
+    if 0 <= confiance <= 1:
+        break # <--- On sort de la boucle ici car la valeur est BONNE
+    
+    print("Mauvaise valeur, réessayez...")
+    time.sleep(1)
+
+# Le reste du code s'exécute après le break
 print("--- Analyse d'image IA ---")
+
 if confiance > 0.80:
     print("Résultat : L'IA est sûre qu'il s'agit d'un chat !")
 elif confiance<0.81 and  confiance>0.65:
@@ -12,3 +24,5 @@ else:
 # Exercice : Change la valeur de 'confiance' sur ton PC 
 # et fais un 'git pull' sur ton tel pour voir le résultat changer !
 
+data = open("data.txt" , "r")
+print("contenu du fichier: \n" + data.read())
